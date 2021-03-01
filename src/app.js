bbaeb8dccc45a5d52008ef85f0fefed81e1fc15c
develiza <67155824+develiza@users.noreply.github.com>
2021-03-01 21:26:05 +0100

    chores: updates styles and script

--- src/app.js
+++ src/app.js
@@ -2,7 +2,18 @@
 // import 'bootstrap';
 
 const addInfoToDom = event => {
+    const ctrlKey = event.ctrlKey;
+    const altKey = event.altKey;
+    if(ctrlKey){ 
+        addKeys.innerHTML = 'control key';
+    } else {
+        addKeys.innerHTML = '&nbsp;';
+    }
+    if(altKey) {
+        addKeys.innerHTML = 'alt key'
+    }
     keyPressed.innerHTML = event.key;
+    
     utf16Code.innerHTML = event.key.charCodeAt();
     keyboardLocale.innerHTML = event.code;
 }
@@ -11,3 +22,4 @@ window.addEventListener("keydown", addInfoToDom);
 const keyPressed = document.querySelector('p[id="keyPressed"]');
 const utf16Code = document.querySelector('p[id="utf16Code"]');
 const keyboardLocale = document.querySelector('p[id="keyboardLocale"]');
+const addKeys = document.querySelector('p[id="addKeys"]');
